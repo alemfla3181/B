cf92e3411e322b60990ec37e9e676b07d0ef548d
KDB <alemfla3181@naver.com>
2022-05-10 19:32:05 +0900

    um,...

---
+++ server/models/Contents.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ContentsSchema = mongoose.Schema({
+
+    writer: {
+        type: Schema.Types.ObjectId,
+        ref:'User'
+    },
+    content: {
+        type: String
+    }
+
+
+}, {timestamps: true})
+
+
+
+const Contents = mongoose.model('Contents', ContentsSchema);
+
+module.exports = { Contents }
