55a5d542ee416a750f4eb311e9a515d476dc6df6
KDB <alemfla3181@naver.com>
2022-05-11 17:47:28 +0900

    login check

--- Client/src/Components/MainBoard.js
+++ Client/src/Components/MainBoard.js
@@ -1,17 +1,35 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import { Link } from 'react-router-dom';
 import Footer from './Footer';
 import Header from './Header';
 
+
+
 function MainBoard() {
+
+    const [User, setUser] = useState(false);
+    
+    useEffect(() => {
+        if (window.sessionStorage.getItem("user_Email", JSON.stringify(User))) {
+            setUser(true);
+        }
+    },[])
+
     return (
         <div>
-            <Header />
-            <div>
-                <button><Link to='/login'>로그인</Link></button>
-                <button><Link to='/register'>회원가입</Link></button>
-            </div>
-            <Footer />
+            {!User ?
+                <div>
+                    <Header />
+                        <div>
+                            <button><Link to='/login'>로그인</Link></button>
+                            <button><Link to='/register'>회원가입</Link></button>
+                        </div>
+                    <Footer />
+                </div>
+                : <div>
+                    fuck
+                </div> 
+        }
         </div>
     )
 }
