1bc5134cf722ddbfc49b93effba08bb11766342c
KDB <alemfla3181@naver.com>
2022-05-12 10:26:19 +0900

    css

--- Client/src/Components/Write.js
+++ Client/src/Components/Write.js
@@ -35,9 +35,7 @@ function Write() {
     return (
       <div className='LandingPage'>
             <Header />
-            <div style={{
-                display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
-            }}>
+            <div>
                 <form style={{ display: 'flex', flexDirection: 'column' }}
                     onSubmit={onSubmitHandler}>
                     <label>Title</label>
